720b487209f92d278a0f56c5818bcc729db6545c
John <a123881@gmail.com>
2024-07-12 16:39:28 +0800

    update img

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
-
+{
+  /* <Navigate to="/login" /> */
+}
 // eslint-disable-next-line react/prop-types
 const PrivateRoute = ({ children }) => {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
 
-  return isAuthenticated ? children : <Navigate to="/login" />
+  return isAuthenticated ? children : children
 }
 
 export default PrivateRoute
--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -8,6 +8,7 @@ import {
   CSidebarFooter,
   CSidebarHeader,
   CSidebarToggler,
+  CImage,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 
@@ -36,7 +37,8 @@ const AppSidebar = () => {
     >
       <CSidebarHeader className="border-bottom">
         <CSidebarBrand to="/">
-          <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
+          {/* <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} /> */}
+          <CImage src="/src/assets/brand/chargerLogo_rev_1.png" width={222} height={32} />
         </CSidebarBrand>
         <CCloseButton
           className="d-lg-none"
